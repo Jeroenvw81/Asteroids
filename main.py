943976074ca070c7107801aeaf796729bd0ef8f9
Rojak <Jeroenvw81@gmail.com>
2025-05-09 23:33:03 +0200

    update to laptop

--- main.py
+++ main.py
@@ -2,20 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import * #imports all of constants.py, not usually done, just the parts that are needed
+from player import PlayerShape
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()                                                           #initializes pygame
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))         #setting window to variables in constants
+    clock = pygame.time.Clock()        
+    player = PlayerShape(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)                                     
+    dt = 0
+    
+
 
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get():                                    #beginning of game loop, will check if user has closed the window and exits loop
             if event.type == pygame.QUIT:
                 return
         
+        
         screen.fill("black")
-        pygame.display.flip()
-    
+        player.draw(screen)        
+                                                
+        pygame.display.flip()                                               #refreshes the "black" screen at each loop
 
-if __name__ == "__main__":
+        dt = clock.tick(60) / 1000                                          #limits fps to 60 /1000 = to convert from ms to s. It will pause the game loop until 1/60th of a second has passed.
+
+if __name__ == "__main__":                                                  #only runs main() if main.py is called directly
     main()
